3f3d4bdde75b5e012ba256147baf1f23c7751281
Aidan Farhi <aidanmcfarhi@gmail.com>
2026-03-11 22:31:22 -0400

    added the FastAPI ai service

--- web-app/main.go
+++ web-app/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
+
 	mux := http.ServeMux{}
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("content-type", "application/json")
 		w.Write([]byte("{\"hello\": \"world\"}"))
 	})
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		resp, _ := http.Get("http://ai-service:8000/api/v1/hello")
+		defer resp.Body.Close()
+		body, _ := io.ReadAll(resp.Body)
+		w.Header().Add("content-type", "application/json")
+		w.Write([]byte(body))
+	})
+
 	s := http.Server{
 		Addr:    ":8090",
 		Handler: &mux,
