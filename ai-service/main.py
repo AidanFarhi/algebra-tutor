53804fc57b2f2cf72b7a56f9564deab475f3293e
Aidan Farhi <aidanmcfarhi@gmail.com>
2026-03-13 16:53:40 -0400

    wired up the ai service to the db

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from psycopg import connect
 
 app = FastAPI()
+
+
+def ping():
+    try:
+        with connect("host=db dbname=algtutor user=postgres password=postgres") as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT 1")
+                print(cur.fetchone())
+    except Exception as e:
+        print(e)
 
 
 @app.get("/api/v1/hello")
 async def hello():
     return {"hello": "world"}
+
+
+@app.get("/api/v1/pingdb")
+async def ping_db():
+    ping()
+    return {"msg": "done"}
