e40a777d1091c7401350943edc0b01c526f002c1
Aidan Farhi <aidanmcfarhi@gmail.com>
2026-03-12 23:29:53 -0400

    added db and connection to db from golang app

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This is an app that uses AI to assist with a user learning Algebra.
 ## To run
 
 `docker compose up --build`
+
+## To tear down
+
+`docker compose down -v`
--- web-app/main.go
+++ web-app/main.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"net/http"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+func TestDBConnection(connString string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	pool, err := pgxpool.New(ctx, connString)
+	if err != nil {
+		return fmt.Errorf("failed to create pool: %w", err)
+	}
+	defer pool.Close()
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		return fmt.Errorf("database ping failed: %w", err)
+	}
+
+	fmt.Println("✅ Successfully connected to Postgres")
+	return nil
+}
+
 func main() {
+
+	// attempt to connect to db
+	connString := "postgres://postgres:postgres@db:5432/algtutor"
+	TestDBConnection(connString)
 
 	mux := http.ServeMux{}
 
@@ -26,5 +54,8 @@ func main() {
 		Addr:    ":8090",
 		Handler: &mux,
 	}
+
+	fmt.Println("server about to start")
+
 	s.ListenAndServe()
 }
